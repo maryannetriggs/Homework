4bed42ca9550525293b9cc7ee9acf66679b8a450
Mary-Anne Triggs <maryannetriggs@gmail.com>
2019-09-11 09:50:10 +0100

    adding hw again

--- maryannet/js-calculator-with-control-flow/solution-code/app.js
+++ maryannet/js-calculator-with-control-flow/solution-code/app.js
@@ -1,60 +1,60 @@
-// General Calculator
+// // General Calculator
 
-// Gathering info from user for calculation
-const mathsOperator = prompt('Please enter the mathmematical operator you wish to use in the calculation. For example: + for addition, - for subtraction, / for division, * for multiplication, type \'power\' for exponentials, type \'root\' for square root calculations ')
-const userNum1 = parseFloat(prompt('Please enter the first number of your calculation'))
-const userNum2 = parseFloat(prompt('Please enter the second number of your calculation (type 0 for square root calculations'))
+// // Gathering info from user for calculation
+// const mathsOperator = prompt('Please enter the mathmematical operator you wish to use in the calculation. For example: + for addition, - for subtraction, / for division, * for multiplication, type \'power\' for exponentials, type \'root\' for square root calculations ')
+// const userNum1 = parseFloat(prompt('Please enter the first number of your calculation'))
+// const userNum2 = parseFloat(prompt('Please enter the second number of your calculation (type 0 for square root calculations'))
 
-// console.log(userNum1, typeof userNum1)
-// console.log(userNum2, typeof userNum2)
-// console.log(mathsOperator, typeof mathsOperator)
+// // console.log(userNum1, typeof userNum1)
+// // console.log(userNum2, typeof userNum2)
+// // console.log(mathsOperator, typeof mathsOperator)
 
-// Mathematical calculations
-const add = (userNum1 + userNum2)
-const subtract = (userNum1 - userNum2)
-const multiply = (userNum1 * userNum2)
-const divide = (userNum1 / userNum2)
-const power = Math.pow(userNum1, userNum2)
-const root = Math.sqrt(userNum1)
+// // Mathematical calculations
+// const add = (userNum1 + userNum2)
+// const subtract = (userNum1 - userNum2)
+// const multiply = (userNum1 * userNum2)
+// const divide = (userNum1 / userNum2)
+// const power = Math.pow(userNum1, userNum2)
+// const root = Math.sqrt(userNum1)
 
-// Determining operator and returning calculation via an alert
-if (mathsOperator === '+') {
-  alert(`${userNum1} ${mathsOperator} ${userNum2} equals ${add}`)
-} else if (mathsOperator === '-') {
-  alert(`${userNum1} ${mathsOperator} ${userNum2} equals ${subtract}`)
-} else if (mathsOperator === '*') {
-  alert(`${userNum1} ${mathsOperator} ${userNum2} equals ${multiply}`)
-} else if (mathsOperator === '/') {
-  alert(`${userNum1} ${mathsOperator} ${userNum2} equals ${divide}`)
-} else if (mathsOperator === 'power') {
-  alert(`${userNum1} to the ${mathsOperator} of ${userNum2} equals ${power}`)
-}  else if (mathsOperator === 'root') {
-  alert(`the square root of ${userNum1} equals ${root}`)
-} else {
-  alert('invalid entry, please try again')
-}
+// // Determining operator and returning calculation via an alert
+// if (mathsOperator === '+') {
+//   alert(`${userNum1} ${mathsOperator} ${userNum2} equals ${add}`)
+// } else if (mathsOperator === '-') {
+//   alert(`${userNum1} ${mathsOperator} ${userNum2} equals ${subtract}`)
+// } else if (mathsOperator === '*') {
+//   alert(`${userNum1} ${mathsOperator} ${userNum2} equals ${multiply}`)
+// } else if (mathsOperator === '/') {
+//   alert(`${userNum1} ${mathsOperator} ${userNum2} equals ${divide}`)
+// } else if (mathsOperator === 'power') {
+//   alert(`${userNum1} to the ${mathsOperator} of ${userNum2} equals ${power}`)
+// }  else if (mathsOperator === 'root') {
+//   alert(`the square root of ${userNum1} equals ${root}`)
+// } else {
+//   alert('invalid entry, please try again')
+// }
 
 
 
-// BMI Calculator
+// // BMI Calculator
 
-// Gathering info from user for calculation
-const imperialOrMetric = prompt('Do you want to calculate your BMI with imperial or metric figures?')
-const weight = parseFloat(prompt('Please enter your weight (metric - kg or imperial - lb)'))
-const height = parseFloat(prompt('Please enter your height (metric - m or imperial - in)'))
+// // Gathering info from user for calculation
+// const imperialOrMetric = prompt('Do you want to calculate your BMI with imperial or metric figures?')
+// const weight = parseFloat(prompt('Please enter your weight (metric - kg or imperial - lb)'))
+// const height = parseFloat(prompt('Please enter your height (metric - m or imperial - in)'))
 
-console.log(imperialOrMetric, typeof userNum1)
-console.log(weight, typeof userNum2)
-console.log(height, typeof mathsOperator)
+// console.log(imperialOrMetric, typeof userNum1)
+// console.log(weight, typeof userNum2)
+// console.log(height, typeof mathsOperator)
 
-// Mathematical calculations and output
-if (imperialOrMetric === 'metric') {
-  const bmi = (weight / (height * height))
-  alert(`Your BMI is ${bmi}`)
-} else {
-  const bmi = (weight / (height * height)) * 703
-  alert(`Your BMI is ${bmi}`)
-}
+// // Mathematical calculations and output
+// if (imperialOrMetric === 'metric') {
+//   const bmi = (weight / (height * height))
+//   alert(`Your BMI is ${bmi}`)
+// } else {
+//   const bmi = (weight / (height * height)) * 703
+//   alert(`Your BMI is ${bmi}`)
+// }
 
 
 
