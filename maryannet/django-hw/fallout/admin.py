d98498197a7b3256b062120881e2ccaf58e9cf12
Mary-Anne Triggs <maryannetriggs@gmail.com>
2019-11-11 10:06:54 +0000

    adding hw

--- maryannet/django-hw/fallout/admin.py
+++
@@ -1,7 +0,0 @@
-class Fallout(models.Model):
-    title = models.CharField(max_length=50)
-    author = models.CharField(max_length=50)
-    image = models.CharField(max_length=200)
-
-    def __str__(self):
-        return f'{self.title} - {self.author}'
